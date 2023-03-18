5bb1fd2900e5c2547b8fd348978e142ade33b6c2
RitwikGanguly <gangulyritwik2003@gmail.com>
2023-03-19 01:57:38 +0530

    added

--- pages/1_Movie_Recommendation.py
+++ pages/1_Movie_Recommendation.py
@@ -41,8 +41,7 @@ butt = st.button("Yeah!!!")
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-driver=webdriver.Chrome(executable_path=r'C:\Users\RITWIK GANGULY\anaconda3\chromedriver.exe')
-driver.maximize_window()
+driver = webdriver.Chrome(executable_path=r'C:\Users\RITWIK GANGULY\anaconda3\chromedriver.exe')
 
 def img_url(a):
     p = []
